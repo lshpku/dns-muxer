d1d81bae3abe036566ae5f20dd21c2aeca47b207
Liang Shuhao <liangshuhao@pku.edu.cn>
2024-07-28 01:09:49 +0800

    Add main.go

---
+++ main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"flag"
+)
+
+var (
+	flagListen    = flag.String("listen", "", "serve DNS at this address")
+	flagListenUDP = flag.String("listen-udp", "", "serve DNS at this address (for UDP)")
+	flagListenTCP = flag.String("listen-tcp", "", "serve DNS at this address (for TCP)")
+	flagQueryCN   = flag.String("query-cn", "", "query CN at this address")
+	flagFwdLocal  = flag.String("forward-local", "", "local DNS address")
+	flagFwdProxy  = flag.String("forward-proxy", "", "DoT proxy address")
+)
+
+func main() {
+	flag.Parse()
+
+	chooseFirstAvail := func(a, b string) string {
+		if a != "" {
+			return a
+		}
+		return b
+	}
+
+	hasListener := false
+	if addr := chooseFirstAvail(*flagListenUDP, *flagListen); addr != "" {
+		startUDPListener(addr)
+		hasListener = true
+	}
+	if addr := chooseFirstAvail(*flagListenTCP, *flagListen); addr != "" {
+		startTCPListener(addr)
+		hasListener = true
+	}
+	if !hasListener {
+		log.Fatal("No listener is specified")
+	}
+
+	startDoTHandler()
+
+	select {}
+}
