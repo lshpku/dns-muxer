c86b3fd4786f92d9110064a93b138a8bc65f2c94
Liang Shuhao <liangshuhao@pku.edu.cn>
2024-07-28 13:46:37 +0800

    Add dot.go

---
+++ dot.go
@@ -0,0 +1,146 @@
+package main
+
+import (
+	"crypto/tls"
+	"encoding/binary"
+	"errors"
+	"net"
+	"sync/atomic"
+)
+
+// DoTQuery represents a query that is sent to the DoT server.
+type DoTQuery struct {
+	payload  []byte
+	callback func([]byte, error)
+	retry    int
+}
+
+var DoTChan = make(chan *DoTQuery, 16)
+
+func makeDoTQuery(payload []byte, callback func([]byte, error)) {
+	query := &DoTQuery{
+		payload:  make([]byte, 2+len(payload)),
+		callback: callback,
+		retry:    3,
+	}
+	binary.BigEndian.PutUint16(query.payload, uint16(len(payload)))
+	copy(query.payload[2:], payload)
+	DoTChan <- query
+}
+
+type DoTClient struct {
+	conn *tls.Conn
+	// Queries that are sent and haven't been replied.
+	// Note: it can only be closed from the writer side.
+	queries chan *DoTQuery
+	closed  atomic.Bool
+}
+
+func (c *DoTClient) runReader() {
+	var err error
+
+	// Read replies and callback on queries
+	for {
+		var payload []byte
+		payload, err = readTCPQuery(c.conn)
+		if err != nil {
+			break
+		}
+
+		if query := <-c.queries; query != nil {
+			go query.callback(payload, nil)
+		} else {
+			err = errors.New("DoT reply has no matching query")
+			break
+		}
+	}
+
+	if !c.closed.Swap(true) {
+		log.Info("DoT reader closed:", err)
+	}
+	c.conn.Close()
+
+	// Retry unfinished queries
+	for query := range c.queries {
+		retryQuery(query)
+	}
+}
+
+func newDoTClient() (*DoTClient, error) {
+	// Dial socks proxy
+	rawConn, err := net.Dial("tcp", *flagFwdProxy)
+	if err != nil {
+		return nil, err
+	}
+	if err := socks5Handshake(rawConn, "dns.google", 853); err != nil {
+		rawConn.Close()
+		return nil, err
+	}
+
+	// Do TLS handshake
+	conn := tls.Client(rawConn, &tls.Config{
+		ServerName: "dns.google",
+	})
+	if err := conn.Handshake(); err != nil {
+		conn.Close()
+		return nil, err
+	}
+
+	client := &DoTClient{
+		conn:    conn,
+		queries: make(chan *DoTQuery, 8),
+	}
+	go client.runReader()
+
+	log.Info("new DoT client")
+	return client, nil
+}
+
+func retryQuery(query *DoTQuery) {
+	query.retry--
+	if query.retry <= 0 {
+		query.callback(nil, errors.New("Max retries exceeded"))
+		return
+	}
+	select {
+	case DoTChan <- query:
+		log.Info("retry DoT query")
+	default:
+		query.callback(nil, errors.New("DoT channel is full"))
+	}
+}
+
+func runDoTClient() {
+	var client *DoTClient
+
+	for query := range DoTChan {
+		log.Debug("handling query", len(query.payload))
+
+		// Create a client if there is no client, or if the current client has
+		// been closed.
+		if client == nil || client.closed.Load() {
+			if client != nil {
+				close(client.queries)
+			}
+			var err error
+			client, err = newDoTClient()
+			if err != nil {
+				retryQuery(query)
+				continue
+			}
+		}
+
+		// Try to forward the query.
+		// Close the client if Write fails.
+		if _, err := client.conn.Write(query.payload); err == nil {
+			log.Debug("sending query", len(query.payload))
+			client.queries <- query
+		} else {
+			if !client.closed.Swap(true) {
+				log.Info("DoT writer closed:", err)
+			}
+			client.conn.Close()
+			retryQuery(query)
+		}
+	}
+}
